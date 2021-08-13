83c01792e28d33c4e208e7aeb88119647cf08ac4
chainvisions <chainvisions@outlook.com>
2021-08-12 22:33:26 -0600

    Update IController

--- contracts/interfaces/IController.sol
+++ contracts/interfaces/IController.sol
@@ -1,5 +1,8 @@
-pragma solidity 0.8.6;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
 
 interface IController {
     function owner() external view returns (address);
+    function storageContract() external view returns (address);
+    function dynamicStorage() external view returns (address);
 }
